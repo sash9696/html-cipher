7bb73d7e078b20a0d01d6d76c48547ebb6a70728
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-12-05 09:02:42 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+//single line comment
+//console.log(3 + 1)
+
+/*console.log(
+    3
+    +
+     1) multiline comment */
+
+// alert('Lets study js')
+// alert('Hi everyone')
+
+//Variable
+
+// let message; //variable declaration
+// message = 'hello' //variable initialization
+// alert(message)
+
+// let message = 'hello world'
+// alert(message)
+
+// let user = 'john',
+//     age = 20,
+//     message = 'hello';
+
+// console.log(user, age, message)
+
+
+// const userBirthday = '18/05/2000';
+// userBirthday = 'abcsdklsan'
+
+
+//Data Types
+//8 types of datatypes
+
+//Number
+let n = 123
+n = 20.1234
+//a number can represent both integer and floating point numbers
+
+//special numeric values 
+
+// alert(1 / 0) //Infinity
+
+// alert('text' / 3)  NaN
+
+// var a = '20'
+// var b = 2
+
+
+//BigInt
+//(2power53 - 1) , -(2power53 - 1)
+
+// const bigInt = 31827638126382716381263812638161326813618368126338126381263812638126312836821n
+
+// console.log(typeof bigInt)
+
+//String
+//Boolean
+//null
+//null pointer, nothing, empty, the value is unknown
+// let age = null;
+
+// console.log(typeof age)
+
+//undefined
+//value is not assigned
+let age;
+
+alert(age)
+
+
+
+//objects
