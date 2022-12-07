bef4f33459676193ec798b78fbb3f2b47c87b1fb
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-12-07 08:59:32 +0530

    Add files via upload

---
+++ js/conditionals.js
@@ -0,0 +1,18 @@
+
+
+
+
+let year = 2019
+
+if (year == 2022) {
+    console.log('You are correct')
+} else if (year > 2020) {  //else is optional
+    console.log('You are close')
+
+} else if (year > 2018) {
+    console.log('Try again you have a chance')
+
+} else {
+    console.log('You are wrong')
+
+}
---
+++ js/loops.js
@@ -0,0 +1,68 @@
+
+//while and for loops
+
+//while loop
+//  while(conditon){
+//     //code 
+//     //body of the loop
+//  }
+
+
+
+// let i = 0
+
+// while (i < 5){
+//     console.log(i)
+//     i++
+
+// }
+
+// let x= 5
+
+// while (i != 0){
+//     console.log(i)
+//     i = i - 1
+// }
+
+//do .. while loop
+
+// let i = 0
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 5)
+
+
+while (false) {
+    console.log('hello world')
+}
+
+do {
+    console.log('hello world')
+
+} while (false)
+
+
+// for(begin; condition; step){
+
+// }
+
+
+// for (let i = 0; i < 5; i++){
+//     console.log(i)
+// }
+
+// for (let i = 5; i > 0; i--){
+//     console.log(i)
+// }
+
+// let i = 0
+
+// for (; i < 3; i++){
+//     console.log(i)
+// }
+// let i = 0
+
+// for (; i < 3; ){
+//     console.log(i++)
+// }
